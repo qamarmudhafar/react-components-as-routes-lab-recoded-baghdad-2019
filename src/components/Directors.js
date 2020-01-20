4ca2ab3b6d46642359ec5ab03961e711de1d51f8
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-20 10:43:21 +0000

    Automatically backed up by Learn

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,13 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Directors Page</h1>
+    {directors.map(director =>
+      <div>
+        <h1>{director.name}</h1>
+        <ul>{director.movies.map(data =>
+          <li>{data}</li>)}</ul>
+      </div>)}
     </div>
   );
 }
