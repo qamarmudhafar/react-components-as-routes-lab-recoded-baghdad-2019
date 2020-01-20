5af1c6867298e473d649b0f395ce4f86faff38dd
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-20 10:44:28 +0000

    Automatically backed up by Learn

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+const link = {
+  width: '100px',
+  padding: '12px',
+  margin: '0 6px 6px',
+  background: 'blue',
+  textDecoration: 'none',
+  color: 'white',
+}
+
+
 const NavBar = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <div className="navbar">
+    <NavLink to="/"  style={link} activStyle={{ background: 'darkblue' }}>Home</NavLink>
+    <NavLink to="/movies" exact style={link} activStyle={{ background: 'darkblue' }}>Movies</NavLink>
+    <NavLink to="/directors" exact style={link} activStyle={{ background: 'darkblue' }}>Directors</NavLink>
+    <NavLink to="/actors" exact style={link} activStyle={{ background: 'darkblue' }}>Actors</NavLink>
+    </div>
     </div>
   );
 };
