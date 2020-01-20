55b497ee9a80bb35cca22f4d7313afac5bcb30cc
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-20 10:43:01 +0000

    Automatically backed up by Learn

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -4,7 +4,14 @@ import { actors } from '../data';
 const Actors = () => {
   return (
     <div>
-      {/*{code here}*/}
+    <h1>Actors Page</h1>
+      {actors.map(actor =>
+        <div>
+          <h1>{actor.name}</h1>
+          <ul>{actor.movies.map(data =>
+            <li>{data}</li>
+          )}</ul>
+        </div>)}
     </div>
   );
 };
